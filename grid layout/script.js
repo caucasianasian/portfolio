c4a98bc083aec5827804f8e8bd0ae41a190ffdc9
Chris Maton <chrismaton@naver.com>
2017-05-13 20:50:54 +0900

    Update script.js

--- grid layout/script.js
+++ grid layout/script.js
@@ -1,7 +1,7 @@
-var original = document.getElementById("original"),
-    percent = document.getElementById("percent"),
-    calculate = document.getElementById("calculate"),
-    output = document.getElementById("output");
+var original = document.getElementById("original");
+var percent = document.getElementById("percent");
+var calculate = document.getElementById("calculate");
+var output = document.getElementById("output");
 
 calculate.addEventListener("click", function(){
   output.textContent = original.value - (original.value * ("0."+percent.value));
