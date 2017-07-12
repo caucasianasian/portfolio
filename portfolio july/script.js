8fcb861a9fb43281ad6c7caf79431647ec07750c
Chris Maton <chrismaton@naver.com>
2017-07-12 17:00:09 +0900

    Add files via upload

---
+++ portfolio july/script.js
@@ -0,0 +1,7 @@
+// GLOBAL VARIABLES (PROJECTS FROM DOM)
+const marketingBTN = document.getElementById("marketingBTN");
+const marketingProjects = document.querySelectorAll(".marketing");
+
+marketingBTN.addEventListener("click", function(){
+	alert("test");
+})
